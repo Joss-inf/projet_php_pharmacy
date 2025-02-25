85b016ff275b0cdda7f7aa38222c298d39f68652
Romain <romain.deschaseaux@outlook.fr>
2025-02-25 14:55:10 +0100

    update search and add map for each pharmacy

--- header/header.php
+++ header/header.php
@@ -5,8 +5,8 @@
     <meta charset="UTF-8">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <script src="https://cdn.tailwindcss.com"></script>
-    <link rel="stylesheet" href="https://unpkg.com/leaflet/dist/leaflet.css" />
-    <script src="https://unpkg.com/leaflet/dist/leaflet.js"></script>
+    <link rel="stylesheet" href="https://unpkg.com/leaflet@1.7.1/dist/leaflet.css" />
+    <script src="https://unpkg.com/leaflet@1.7.1/dist/leaflet.js"></script>
 </head>
 <body class="bg-gray-100">
 <!-- Header section with dark background and white text -->
@@ -31,7 +31,7 @@
       <!-- Icons container for search, profile, and shopping cart -->
       <div class="flex space-x-4">
         <!-- Search icon -->
-        <a href="#" class="hover:text-gray-300">
+        <a href="search.php" class="hover:text-gray-300">
           <i class="fas fa-magnifying-glass"></i>
         </a>
         <!-- Profile icon -->
