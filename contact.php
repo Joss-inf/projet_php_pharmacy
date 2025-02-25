c35b2acb91f9c44fb4178e55da335646ad3c3315
AmandineGUISY <aguisy@yahoo.fr>
2025-02-25 14:58:52 +0100

    Feature/ContactPage Finalisation of the api. The form is now sending a mail to the contact mail of the platform

--- contact.php
+++ contact.php
@@ -1,35 +1,36 @@
 <?php 
 
-require "header/header.php"
+require "header/header.php";
 
 ?>
 
 <body class="bg-gray-100 font-sans antialiased">
-    <!-- Conteneur principal -->
     <div class="max-w-4xl mx-auto my-16 p-8 bg-white shadow-lg rounded-lg">
-        <!-- Titre -->
-        <h1 class="text-4xl font-bold text-center text-gray-800 mb-8">Nous Contacter</h1>
-        <h3 class="text-1xl text-center text-gray-800 mb-8">(Merci de renseigner votre numéro de téléphone ou votre email dans votre message)</h3>
-        
-        <!-- Formulaire de contact -->
-        <form action="#" method="POST">
-            <div class="grid gap-6">
-                <input type="text" id="searchInput" placeholder="Nom de la pharmacie" class="mt-2 p-3 w-full border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-600">
-                <div id="pharmacyEmail"></div>
 
-                <div>
-                    <label for="object" class="block text-lg font-medium text-gray-700">Objet :</label>
-                    <input type="text" id="name" name="name" required class="mt-2 p-3 w-full border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-600">
-                </div>
-                
-                <div>
-                    <label for="message" class="block text-lg font-medium text-gray-700">Message</label>
-                    <textarea id="message" name="message" rows="6" required class="mt-2 p-3 w-full border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-600"></textarea>
-                </div>
+        <h1 class="text-5xl font-bold text-center text-gray-800 mb-2">Nous Contacter</h1>
+        <h3 class="text-1xl text-center text-gray-800 mb-5">Merci de bien vouloir formuler votre message de manière claire et professionnelle afin de garantir un traitement rapide et efficace de votre demande.</h3>
+        
+        <form action="https://formspree.io/f/xldgyall" method="POST">
+            <div class="mt-4">
+                <input type="text" name="Mail" required class="mt-2 p-3 w-full border border-gray-300 rounded-md shadow-sm" placeholder="Mail">
             </div>
-            
-            <div class="mt-6 text-center">
-                <button type="submit" class="px-6 py-3 text-lg font-semibold text-white bg-blue-500 hover:bg-blue-600 rounded-md shadow-md transition duration-200">Envoyer</button>
+
+            <div class="mt-4">
+                <input type="text" name="phone" required class="mt-2 p-3 w-full border border-gray-300 rounded-md shadow-sm" placeholder="Numéro De Téléphone">
+            </div>
+
+            <div class="mt-10">
+                <label for="objet" class="block text-lg font-medium text-gray-700">Objet :</label>
+                <input type="text" name="Object" required class="mt-2 p-3 w-full border border-gray-300 rounded-md shadow-sm">
+            </div>
+
+            <div class="mt-6">
+                <label for="message" class="block text-lg font-medium text-gray-700">Message</label>
+                <textarea id="message" rows="6" name="text" required class="mt-2 p-3 w-full border border-gray-300 rounded-md shadow-sm"></textarea>
+            </div>
+
+            <div class="mt-7 text-center">
+                <button type="submit" class="px-6 py-3 text-lg font-semibold text-white bg-green-600 hover:bg-green-700 rounded-md shadow-md transition duration-200">Envoyer</button>
             </div>
         </form>
     </div>
