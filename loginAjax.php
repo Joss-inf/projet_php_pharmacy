ef406ecb4cda01d951e0e6ca44f3dc238c431916
Joss-inf <jossbessi460@gmail.com>
2025-02-26 10:21:52 +0100

    adding login Ajax

---
+++ loginAjax.php
@@ -0,0 +1,24 @@
+<?php
+session_start();
+require_once 'User.php';
+require_once 'database.php';
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+$postData = json_decode(file_get_contents('php://input'), true);
+// Récupération des données
+$password = $postData['password'] ?? '';
+$email = $postData['email'] ?? '';
+$u = new User(Database::getConnection());
+$res = $u -> login($email,$password);
+if(!isset($res[3]) && $res[0] == 200){
+    
+    $_SESSION['user_id'] = $res ['id'] ?? '';
+    $_SESSION['email'] = $res ['email']?? '';
+    $_SESSION['role'] = $res ['role']?? '';
+    $_SESSION['id_pharmacy'] = $res['pharmacy_id'] ?? '';
+    $msg = [$res[0],$res[1]];
+    echo json_encode(['succes' => 'succes', 'message'=> $msg]);
+}else{
+    echo json_encode(['succes' => 'succes', 'message'=> $res]);
+}
+exit;
+}
