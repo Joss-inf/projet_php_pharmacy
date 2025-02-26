539fa9b0472b2078734a571f54441fcef5b25013
Joss-inf <jossbessi460@gmail.com>
2025-02-26 10:20:04 +0100

    additionnal html dynamic code

--- login.php
+++ login.php
@@ -4,17 +4,20 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
+    <script src="login.js" async></script>
     <title>Page de Connexion</title>
 </head>
 <body class="bg-gray-100 flex items-center justify-center h-screen">
+
     <div class="w-full max-w-md">
-        <form action="login.php" method="post" class="bg-black shadow-md rounded px-8 pt-6 pb-8 mb-4">
+        <form id="loginForm" method="POST" class="bg-black shadow-md rounded px-8 pt-6 pb-8 mb-4">
             <h2 class="text-gray-100 mb-6 text-center text-2xl font-bold">Connexion</h2>
+            <div id="response-container"></div>
             <div class="mb-4">
                 <label class="block text-gray-300 text-sm font-bold mb-2" for="username">
-                    Nom d'utilisateur
+                    email
                 </label>
-                <input class="shadow appearance-none border rounded w-full py-1 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="text" name="username" placeholder="Nom d'utilisateur">
+                <input class="shadow appearance-none border rounded w-full py-1 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="email" type="text" name="email" placeholder="email@exemple.com">
             </div>
             <div class="mb-6">
                 <label class="block text-gray-300 text-sm font-bold mb-2" for="password">
@@ -23,11 +26,11 @@
                 <input class="shadow appearance-none border rounded w-full py-1 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="password" type="password" name="password" placeholder="********">
             </div>
             <div class="flex items-center justify-between">
-                <button class="bg-gray-200  hover:bg-gray-400 text-black font-bold py-1 px-4 rounded focus:outline-none focus:shadow-outline" type="submit">
+                <button class="bg-gray-200  hover:bg-gray-400 text-black font-bold py-1 px-4 rounded focus:outline-none focus:shadow-outline" type="submit" id = 'login'>
                     Se connecter
                 </button>
                 <a class="inline-block align-baseline font-bold text-sm text-gray-200 hover:text-gray-400" href="register.php">
-                    Mot de passe oublié ?
+                    Enregistré vous !
                 </a>
             </div>
         </form>
