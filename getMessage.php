1d854bd9e45abd8e0a1e78028d9d501dc2338a0f
AmandineGUISY <aguisy@yahoo.fr>
2025-02-27 08:19:03 +0100

    Feature/Messages addition of a function who use the getMessage frome the massage class and json encode it

---
+++ getMessage.php
@@ -0,0 +1,19 @@
+<?php 
+// session_start();
+
+require_once "Message.php";
+require_once "database.php";
+
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+$messages = new Messages(Database::getConnection());
+
+$response = $messages->getMessages();
+
+echo json_encode([
+    'status' => $response[0],  // 200 if Ok or 400 if not
+    'message' => $response[1] // the error message
+]);
+?>
