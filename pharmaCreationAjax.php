a09b7f45ca1a00c4180ac8f51c679155d7b2b4ee
AmandineGUISY <aguisy@yahoo.fr>
2025-02-28 03:50:12 +0100

    Feaure/pharmaCreation implementation of the Ajax file for the cration of pharma

---
+++ pharmaCreationAjax.php
@@ -0,0 +1,23 @@
+<?php
+require "PharmacyClass.php";
+require "database.php";
+
+$pharmacy = new Pharmacy(Database::getConnection());
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = trim($_POST['name']);
+    $email = trim($_POST['email']);
+    $phone = trim($_POST['phone']);
+    $address = trim($_POST['address']);
+    $country = trim($_POST['country']);
+    $department = trim($_POST['department']);
+    $description = trim($_POST['description']);
+
+    $response = $pharmacy->postPharmacy($name, $email, $phone, $address, $country, $department, $description);
+    echo json_encode([
+        'status' => $response[0],  // 200 if Ok or 400 if not
+        'message' => $response[1] // the error message
+    ]);
+}
+
+?>
