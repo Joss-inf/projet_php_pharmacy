401208853a36a13f69b8a9a7c4056fe9162660fb
heidi_irdrj <heidi.iroudayaradjou@gmail.com>
2025-02-28 14:04:58 +0100

    Add files via upload

--- logout.php
+++ logout.php
@@ -1,7 +1,17 @@
 <?php
+require_once 'databaseprofile.php';
+
 session_start();
+
+session_unset();
+
 session_destroy();
-header("Location: login.php");
+
+header('Location: login.php');
+
+$db = Database::getInstance();
+$conn = $db->closeConnection();
+
+
 exit();
 ?>
-
